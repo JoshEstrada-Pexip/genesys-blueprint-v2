1a760a73a0a5b46593295112b0e3b5de895a12f2
Josh-Estrada <josh.est456@gmail.com>
2026-01-02 13:55:44 -0700

    Update vite config path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pexip-genesys-app-example/',
+  base: '/genesys-blueprint-v2/',
   plugins: [basicSsl(), react()],
   server: {
     port: 3000,
